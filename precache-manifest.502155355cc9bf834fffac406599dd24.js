498a88b8c1fb4071c800f88c750201f715138709
uwaifo <mr.uwaifo@gmail.com>
2019-05-15 14:16:42 +0100

    Updates

---
+++ precache-manifest.502155355cc9bf834fffac406599dd24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebfa9cce9dd7dba192c4",
+    "url": "/fcccalculatorapp/static/css/main.bd7fc307.chunk.css"
+  },
+  {
+    "revision": "ebfa9cce9dd7dba192c4",
+    "url": "/fcccalculatorapp/static/js/main.04618f1f.chunk.js"
+  },
+  {
+    "revision": "33aeb04775102ef8aa22",
+    "url": "/fcccalculatorapp/static/js/runtime~main.57c95c51.js"
+  },
+  {
+    "revision": "df6cf2a6e4f68442c1d1",
+    "url": "/fcccalculatorapp/static/js/2.76ee5774.chunk.js"
+  },
+  {
+    "revision": "2523ec034e8cedc83690bd53b1c33fb4",
+    "url": "/fcccalculatorapp/index.html"
+  }
+];
